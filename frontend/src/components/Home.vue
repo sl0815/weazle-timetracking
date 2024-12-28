7d15e74f28e0ebfe72793c6cbd31816dc90a9391
sl0815 <sl0815@users.noreply.github.com>
2024-12-28 11:03:23 +0100

    Added a gradient background to the frontend app.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,1 @@
-<template>
-  <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500">
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </div>
-</template>
+<template></template>
